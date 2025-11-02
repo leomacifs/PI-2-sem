3ebd06c61dd5dc7f06e50303229cf47e75ceade7
bMaia07 <brendamaia.bergamasco@gmail.com>
2025-11-02 13:44:13 -0300

    commit

--- CodigosSistema/bibliotecario/backend/async function cadastrarLivro.js
+++
@@ -1,8 +0,0 @@
-async function cadastrarLivro(dados) {
-  const resposta = await fetch('/livros', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(dados)
-  });
-  return await resposta.json();
-}
